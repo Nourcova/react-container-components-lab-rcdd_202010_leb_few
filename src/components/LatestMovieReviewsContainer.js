70b480f01b69ff298ff4608e80b17a1e9827a7b1
Ibrahim Nour <ibrahamnour22@gmail.com>
2021-01-19 23:38:33 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -10,6 +10,8 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
 class LatestMovieReviewsContainer extends Component{
   constructor(){
     super();
-    this.state
+    this.state={
+      
+    }
   }
 }
